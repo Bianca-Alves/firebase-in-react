d06faffaee4e482aa53b3f88a2753cf84f0f114f
Bianca Alves <biasantosalves04@gmail.com>
2022-11-15 00:08:04 -0300

    codando a tela detalhes

--- src/componentes/cardAluno/index.js
+++ src/componentes/cardAluno/index.js
@@ -1,17 +1,27 @@
 import { Text, Image, View, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import React from "react";
 
 export default function CardAluno({imagem, nome, nota1, nota2, nota3}) {
 
+    const navigation = useNavigation();
+
+    function irDetalhes(){
+        navigation.navigate('Detalhes', {imagem:imagem, nome:nome, nota1:nota1, nota2:nota2, nota3:nota3});
+    }
+
     return (
 
-        <View style={styles.container}>
-            <Image source={{ uri: imagem }} style={styles.imagem} />
-            <Text style={styles.text}>{nome}</Text>
-            <Text style={styles.text}>{nota1}</Text>
-            <Text style={styles.text}>{nota2}</Text>
-            <Text style={styles.text}>{nota3}</Text>
-        </View>
+        <TouchableOpacity onPress={irDetalhes} style={styles.container}>
+            <View style={styles.element}>
+                <Image source={{ uri: imagem }} style={styles.imagem} />
+                <Text style={styles.text}>{nome}</Text>
+                <Text style={styles.text}>{nota1}</Text>
+                <Text style={styles.text}>{nota2}</Text>
+                <Text style={styles.text}>{nota3}</Text>
+            </View>
+        </TouchableOpacity>
 
     );
 
@@ -20,23 +30,30 @@ export default function CardAluno({imagem, nome, nota1, nota2, nota3}) {
 const styles = StyleSheet.create({
 
     container: {
-        width: '100%',  
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-around'
+        width: '100%'
     },
 
     imagem: {
         width: 100,
         height: 100,
-        marginBottom: 10
+        borderRadius: '10px'
     },
 
     text: {
         width: '100%',
         textAlign: 'center',
         margin: 15
+    },
+
+    element: {
+        backgroundColor: 'white',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        margin: '10px',
+        padding: '20px',
+        borderRadius: '10px'
     }
 
 })
--- src/paginas/detalhes/index.js
+++ src/paginas/detalhes/index.js
@@ -1,25 +1,145 @@
 import { useRoute } from '@react-navigation/native';
-import React from 'react'
-
-import {View,Text} from 'react-native'
-
-
-
+import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import React from 'react';
 
 export default function Detalhes(){
-const routes = useRoute();
-return(
 
-    <View>
+    const route = useRoute();
 
-    <Text> Detalhes </Text>
-    <Text>{routes.params?.nome}</Text>
-    <Text>{routes.params?.email}</Text>
+    function calcularMedia() {
+        let nota1 = isNaN(route.params?.nota1) ? route.params?.nota1.length : route.params?.nota1
+        let nota2 = isNaN(route.params?.nota2) ? route.params?.nota2.length : route.params?.nota2
+        let nota3 = isNaN(route.params?.nota3) ? route.params?.nota3.length : route.params?.nota3
+    
+        return ((nota1 + nota2 + nota3) / 3)
+    }
+    
+    let Status = <></>
+    
+    if (calcularMedia() > 6) {
+        Status = <Text style={[styles.aprovado, styles.status]}>Aprovado</Text>
+    } else {
+        Status = <Text style={[styles.reprovado, styles.status]}>Reprovado</Text>
+    }
+    
+    return(
 
-    </View>
+        <View style={styles.container}>
+            <View style={{ width: 'auto' }}>
+                <Image source={{ uri: route.params?.imagem }} style={styles.imagem} />
+                <View style={styles.flex}>
+                    <Text style={styles.label}>Nome </Text>
+                    <TextInput style={styles.input} value={route.params?.nome}/>
+                </View>
 
+                <View style={styles.flex}>
+                    <Text style={styles.label}>Nota 1</Text>
+                    <TextInput style={styles.input} value={route.params?.nota1}/>
+                </View>
 
-);
+                <View style={styles.flex}>
+                    <Text style={styles.label}>Nota 2</Text>
+                    <TextInput style={styles.input} value={route.params?.nota2}/>
+                </View>
 
+                <View style={styles.flex}>
+                    <Text style={styles.label}>Nota 3</Text>
+                    <TextInput style={styles.input} value={route.params?.nota3}/>
+                </View>
+
+                {Status}
+
+                <View style={styles.buttons}>
+                    <TouchableOpacity style={[styles.update, styles.button]}>
+                        <Text style={styles.textButton}>ATUALIZAR</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.delete, styles.button]}>
+                        <Text style={styles.textButton}>DELETAR</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+
+    );
 
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+        width: '100%',
+        alignItems: 'center'
+    },
+
+    imagem: {
+        width: 350,
+        height: 220,
+        borderRadius: '10px',
+        marginBottom: '20px'
+    },
+
+    label: {
+        fontWeight: '400',
+        width: '20%'
+    },
+
+    input: {
+        borderColor: 'black',
+        borderWidth: '2px',
+        padding: '2px',
+        width: '80%'
+    },
+
+    flex: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: '20px'
+    },
+
+    status: {
+        fontWeight: 700,
+        fontSize: '20px',
+        textAlign: 'center',
+        marginTop: 10
+    },
+
+    aprovado: {
+        color: 'green',
+    },
+
+    reprovado: {
+        color: 'red',
+    },
+
+    buttons: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        marginTop: '25px'
+    },
+
+    button: {
+        textAlign: 'center',
+        height: 40,
+        width: 100,
+        borderRadius: '3px'
+    },
+
+    update: {
+        backgroundColor: 'green',
+    },
+
+    delete: {
+        backgroundColor: 'red',
+    },
+
+    textButton: {
+        color: 'white',
+        fontWeight: 600,
+        fontSize: 14,
+        lineHeight: 40,
+    }
+
+})
